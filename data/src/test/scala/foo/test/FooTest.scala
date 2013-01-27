ac82782ad3558c5d48121dda0e3fa063e7127ac1
Ismael Juma <ismael@juma.me.uk>
2013-01-27 17:12:12 +0000

    Throw exception during test construction.
    To serve as a test case for SBT issue #653.

--- data/src/test/scala/foo/test/FooTest.scala
+++ data/src/test/scala/foo/test/FooTest.scala
@@ -5,6 +5,8 @@ import org.scalatest.{FunSpec, ShouldMatchers}
 
 class FooTest extends FunSpec with ShouldMatchers {
 
+  sys.error("exception during construction")
+
   describe("Foo.foo should") {
     it("always return 5") {
       Foo.foo should equal (5)
